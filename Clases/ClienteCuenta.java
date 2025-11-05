43da7b693870f5917d28d9f59912e7b5ff3f7e40
JonahtanMG <jmamanigut@unsa.edu.pe>
2025-11-05 14:19:33 -0500

    Se agrego las clases Cuenta

--- Clases/ClienteCuenta.java
+++ Clases/ClienteCuenta.java
@@ -1,4 +1,18 @@
 package Clases;
+import java.util.*;
 class ClienteCuenta {
-    
+    private ArrayList<Cliente> clientes;
+    private Cuenta cuenta;
+    //CONSTRUCTOR
+    public ClienteCuenta(Cuenta cuenta){
+        this.cuenta = cuenta;
+        clientes = new ArrayList<>();
+    }
+    public ClienteCuenta(ArrayList<Cliente> clientes, Cuenta cuenta){
+        this.clientes = clientes;
+        this.cuenta = cuenta;
+    }   
+    public void asociarCliente(Cliente cliente){
+        clientes.add(cliente);
+    }
 }
