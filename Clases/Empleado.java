cfaa7a00906c514ca071ecc2e38fce98594313f2
Ana Miranda <amirandalo@unsa.edu.pe>
2025-11-05 10:34:07 -0500

    Subir clase Empleado

--- Clases/Empleado.java
+++ Clases/Empleado.java
@@ -1,4 +1,36 @@
 package Clases;
-class Empleado {
-    
+import java.util.*;
+
+//Hereda de la clase Persona
+class Empleado extends Persona{
+    private List<String> accionesRealizadas; 
+
+    //Constructor sobrecargado
+    public Empleado (String nombre, String id, String direccion) {
+        super(nombre, id, direccion);
+        this.accionesRealizadas = new ArrayList<>();
+    }
+
+
+    //Método para agregar
+    public void agregarAccion(String accionRealizada) {
+        accionesRealizadas.add(accionRealizada);
+    }
+
+    public void mostrarAcciones() {
+        if (accionesRealizadas.isEmpty()) {
+            System.out.println("No se registraron acciones.");
+        }
+        else {
+            System.out.println("Acciones realizadas:");
+            for (int i=0; i < this.accionesRealizadas.size(); i++) {
+            System.out.println((i+1)+ ". "+ accionesRealizadas.get(i));
+            }
+        }
+    }
+    @Override
+    public void mostrarInformacion () {
+        super.mostrarInformacion();
+        mostrarAcciones();
+    }
 }
