10f1d5ef89ad016009a3dc7019809c35e187368a
Hanna-art543 <amirandalo@unsa.edu.pe>
2025-11-05 15:47:03 -0500

    Corregir errores

--- Clases/ClienteCuenta.java
+++ Clases/ClienteCuenta.java
@@ -1,18 +1,29 @@
 package Clases;
-import java.util.*;
-class ClienteCuenta {
-    private ArrayList<Cliente> clientes;
+
+// Esta clase sirve como "puente" para registrar qué cliente es dueño de qué cuenta
+public class ClienteCuenta {
+    private Cliente cliente;
     private Cuenta cuenta;
-    //CONSTRUCTOR
-    public ClienteCuenta(Cuenta cuenta){
+
+    //Constructor que usa la clase Banco
+    public ClienteCuenta(Cliente cliente, Cuenta cuenta) {
+        this.cliente = cliente;
         this.cuenta = cuenta;
-        clientes = new ArrayList<>();
     }
-    public ClienteCuenta(ArrayList<Cliente> clientes, Cuenta cuenta){
-        this.clientes = clientes;
-        this.cuenta = cuenta;
-    }   
-    public void asociarCliente(Cliente cliente){
-        clientes.add(cliente);
+
+    // Getters que Banco.java necesita para buscar
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public Cuenta getCuenta() {
+        return cuenta;
+    }
+    
+    @Override
+    public String toString() {
+        return "Relacion -> Cliente: " + cliente.getNombre() + 
+               " --- Cuenta: " + cuenta.getIdCuenta() +
+               " (Saldo: " + cuenta.getSaldo() + ")";
     }
 }
