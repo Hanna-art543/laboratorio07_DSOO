e995e9d19be9a430b4c4de9eb982f9a66a2e1e3a
JonahtanMG <jmamanigut@unsa.edu.pe>
2025-11-05 12:40:03 -0500

    Se agrego la clase Cuenta

--- Clases/Cuenta.java
+++ Clases/Cuenta.java
@@ -1,4 +1,48 @@
 package Clases;
+import java.util.*;
 class Cuenta {
-    
+    private String idCuenta;
+    private Double saldo;
+    private ArrayList<Transaccion> Transacciones;
+    //CONSTRUCTOR
+    public Cuenta(String numeroCuenta, Double saldo){
+        this.idCuenta = numeroCuenta;
+        this.saldo = saldo;
+        Transacciones = new ArrayList<>();
+    }
+    //GET Y SET DE ID Y SALDO
+    public String getIdCuenta() {
+        return idCuenta;
+    }
+
+    public void setIdCuenta(String idCuenta) {
+        this.idCuenta = idCuenta;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+    //METODO PARA REGISTRAR UNA TRANSACCION A LA CUENTA
+    public void agregarTransferencia(Transaccion transaccion) {
+        Transacciones.add(transaccion);
+    }
+    //MUESTRA EL SALDO
+    public Double mostrarSaldoActual() {
+        return saldo;
+    }   
+    //IMPRIME EL HISTORIAL DE TRANSACCIONES
+    public void mostrarHistorial() {
+        for(Transaccion objeto : Transacciones){
+            System.out.println(objeto);
+        }
+    }
+    //TOSTRING
+    @Override
+    public String toString() {
+        return "Cuenta{" +
+                "idCuenta='" + idCuenta + '\'' +
+                ", saldo=" + saldo +
+                ", cantidadTransacciones=" + Transacciones.size() +
+                '}';
+    }
 }
